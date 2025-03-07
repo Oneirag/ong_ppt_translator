29a4586202e059802c01025698195cb76a9daeb0
Oscar Neira Garcia <oneirag@yahoo.es>
2025-03-07 10:54:19 +0100

    Adds start and end arguments for __main__.py

--- src/ong_ppt_translator/__main__.py
+++ src/ong_ppt_translator/__main__.py
@@ -1,12 +1,12 @@
-from ong_ppt_translator.traductor_sonnet_v3 import translate_powerpoint
+from ong_ppt_translator.traductor_sonnet_v3 import translate_powerpoint as main
 import argparse
 
-def main(input_file, output_file):
-    translate_powerpoint(input_file, output_file)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--input_file", type=str, required=True)
     args.add_argument("--output_file", type=str, required=True)
+    args.add_argument("--start", type=int, required=False)
+    args.add_argument("--end", type=int, required=False)
     parsed_args = args.parse_args()
-    main(parsed_args.input_file, parsed_args.output_file)
+    main(parsed_args.input_file, parsed_args.output_file, start=parsed_args.start, end=parsed_args.end)
